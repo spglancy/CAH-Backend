c57eb6e23890248ecace2381b7e39f7af64321a5
Sean <Spglancy@gmail.com>
2019-03-19 10:38:43 -0700

    stashing backups

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema
 const UserSchema = new Schema({
   username: String,
   password: String,
+  name: String,
 })
 
 // Salt that password
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const dotenv = require('dotenv').config()
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/cah-backend');
-const app = express();
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/cah-backend');
 const port = 4000
 
 const io = require('socket.io')()
@@ -10,5 +8,7 @@ const io = require('socket.io')()
 const chatSocket = require('./controllers/chat')(io)
 const gameSocket = require('./controllers/game')(io)
 const userSocket = require('./controllers/users')(io)
+const authController = require('./controllers/authController')(io)
+
 
 io.listen(port)
