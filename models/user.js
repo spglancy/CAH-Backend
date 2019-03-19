ef86ccd6484373c0e112c2bac42cda79c1abcfe5
Drake Vorndran <DrakeVorndranOffical@gmail.com>
2019-03-18 17:50:09 -0700

    fixed ; problem

--- models/user.js
+++ models/user.js
@@ -1,31 +1,31 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
   username: String,
-  password: String
-});
+  password: String,
+})
 
 // Salt that password
 UserSchema.pre('save', function (next) {
-  const user = this;
+  const user = this
   if (!user.isModified('password')) {
-    return next();
+    return next()
   }
   bcrypt.genSalt(10, function (err, salt) {
     bcrypt.hash(user.password, salt, function (err, hash) {
-      user.password = hash;
-      next();
-    });
-  });
-});
+      user.password = hash
+      next()
+    })
+  })
+})
 
 // Work that password
 UserSchema.methods.comparePassword = function (password, done) {
   bcrypt.compare(password, this.password, function (err, isMatch) {
-    done(err, isMatch);
-  });
-};
+    done(err, isMatch)
+  })
+}
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", UserSchema)
