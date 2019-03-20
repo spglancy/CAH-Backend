5224a1bae5c60e6a31ec94679a4842d43fea1b08
Drake Vorndran <DrakeVorndranOffical@gmail.com>
2019-03-20 16:19:40 -0700

    everything works, just need to add max score to have a win state

--- controllers/game.js
+++ controllers/game.js
@@ -41,7 +41,7 @@ module.exports = (io) => {
             for (let i = 0; i < 10; i++) {
               cards.push(lobby.whiteCards.splice(Math.floor(Math.random() * lobby.whiteCards.length), 1)[0])
             }
-            lobby.users.push({ name: user.name, id: user._id, points: 0, czar: false, owner, cards, played: false})
+            lobby.users.push({ name: user.name, id: user._id, points: 0, czar: false, owner, cards, played: false })
           }
           lobby.save()
             .then(lobby => {
@@ -62,13 +62,44 @@ module.exports = (io) => {
       Lobby.findById(lobbyId)
         .then(lobby => {
           lobby.gameState = 'Playing'
-          lobby.czar = lobby.users[0].id //Math.floor(Math.random()*lobby.users.length)
-
-          lobby.currBlack = lobby.blackCards.splice(Math.floor(Math.random() * lobby.blackCards.length), 1)[0]
+          lobby.czar = lobby.users[Math.floor(Math.random() * lobby.users.length)].id
+          do {
+            lobby.currBlack = lobby.blackCards.splice(Math.floor(Math.random() * lobby.blackCards.length), 1)[0]
+          }
+          while (lobby.currBlack.pick !== 1)  //temp makes sure all black cards are pick 1
           lobby.save()
             .then(lobby => {
 
               io.to(lobbyId).emit('Update Players', lobby)
+            })
+        })
+    })
+
+    client.on('Select Winner', (lobbyId, card) => {
+      Lobby.findById(lobbyId)
+        .then(lobby => {
+          winner = lobby.playedWhite.reduce((winner, playedCard) => (playedCard.card === card ? playedCard : winner), null)
+          user = lobby.users.reduce((me, user) => (user.id === winner.userId ? user : me), null)
+          user.points += 1
+          lobby.czar = lobby.users[(lobby.users.reduce((reducer, player, index) => (player.id === lobby.czar ? index : reducer), -1) + 1) % lobby.users.length].id
+          lobby.gameState = 'Playing'
+          do {
+            lobby.currBlack = lobby.blackCards.splice(Math.floor(Math.random() * lobby.blackCards.length), 1)[0]
+          }
+          while (lobby.currBlack.pick !== 1)  //temp makes sure all black cards are pick 1
+          lobby.playedWhite = []
+          for (let i = 0; i < lobby.users.length; i++) {
+            lobby.users[i].played = false;
+            lobby.users.set(i, lobby.users[i])
+          }
+          lobby.save()
+            .then(lobby => {
+              Lobby.findByIdAndUpdate(lobbyId, lobby)
+                .then(newLobby => {
+                  console.log(newLobby.users[0].played)
+                  io.to(lobbyId).emit('Update Players', lobby)
+
+                })
             })
         })
     })
@@ -84,12 +115,25 @@ module.exports = (io) => {
             return me
           }, null)
           user.cards.splice(user.cards.indexOf(card), 1)
+          user.cards.push(lobby.whiteCards.splice(Math.floor(Math.random() * lobby.whiteCards.length), 1)[0])
           user.played = true;
           lobby.save()
             .then(lobby => {
               Lobby.findByIdAndUpdate(lobbyId, lobby)
                 .then(newLobby => {
-                  io.to(lobbyId).emit('Update Players', lobby)
+
+                  if (newLobby.playedWhite.length === newLobby.users.length - 1) {
+                    newLobby.gameState = 'Selecting'
+                    console.log('die bitch')
+                    newLobby.save().then(
+                      io.to(lobbyId).emit('Update Players', newLobby)
+
+                    )
+                  }
+                  else {
+                    io.to(lobbyId).emit('Update Players', lobby)
+                  }
+
                 })
                 .catch(err => console.log(err))
 
